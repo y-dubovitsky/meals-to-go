64b56ae1d74eae336970e3a40bc1a652f1a62562
unknown <y.dubovitsky@gmail.com>
2021-12-11 13:58:10 +0300

    123 Solution Finishing Our Restaurant Card

--- src/features/restaurant/components/restaurant-info-card.component.jsx
+++ src/features/restaurant/components/restaurant-info-card.component.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import { View } from 'react-native';
 import { Card } from 'react-native-paper';
 import styled from 'styled-components/native';
+import { SvgXml } from 'react-native-svg';
+import Star from '../../../../assets/star';
+import isOpen from '../../../../assets/isOpen';
 
 const Title = styled.Text`
-  font-family: ${props => props.theme.fonts.body}
+  font-family: ${props => props.theme.fonts.body};
+  font-size: ${props => props.theme.fontSizes.body};
   padding: ${props => props.theme.space[1]};
-  color: ${props => props.theme.colors.ui.primary}
+  color: ${props => props.theme.colors.ui.primary};
 `;
+
+const ClosedTemporary = styled.Text`
+  font-family: ${props => props.theme.fonts.heading};
+  font-style: italic;
+  color: ${props => props.theme.colors.ui.error};
+`
+
+const Rating = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const RestaurantInfo = styled.View`
+  padding: ${props => props.theme.space[1]};
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const RestaurantAddress = styled.Text`
+  font-family: ${props => props.theme.fonts.heading};
+  padding: ${props => props.theme.space[1]};
+  font-size: ${props => props.theme.fontSizes.caption};
+`
 
 const RestaurantCard = styled(Card)`
   backgroundColor: ${props => props.theme.colors.ui.tertiary}
@@ -27,15 +55,36 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
     ],
     address = "St.Petersburg, str.Arbat, h.34",
     isOpenNow = true,
-    rating = 5,
-    isClosedTemporarily
+    rating = 3,
+    isClosedTemporarily = true
   } = restaurant;
 
+  const calculateRestaurantRating = (rating) => {
+    return Array(5).fill(null).map((u, i) => {
+      if (i < rating) return <SvgXml xml={Star} width="20" height="20" />
+    })
+  }
+
+  const showIsRestaurantOpen = (isOpenNow) => {
+    if (isOpenNow) return <SvgXml xml={isOpen} width="20" height="20" />
+  }
+
+  const showIsClosedTemporarily = (isClosedTemporarily) => {
+    if (isClosedTemporarily) return <ClosedTemporary>Temporary Closed</ClosedTemporary>
+  }
 
   return (
     <RestaurantCard>
       <RestaurantCardCover source={{ uri: photos[0] }} />
       <Title>{name}</Title>
+      <RestaurantInfo>
+        <Rating>
+          {calculateRestaurantRating(rating)}
+        </Rating>
+        {showIsClosedTemporarily(isClosedTemporarily)}
+        {showIsRestaurantOpen(isOpenNow)}
+      </RestaurantInfo>
+      <RestaurantAddress>{address}</RestaurantAddress>
     </RestaurantCard>
   );
 }
